9b4e28faed5a2f39eb28e7e47d58d73f2a911916
aydossari <aymalmahdi@gmail.com>
2025-05-05 19:08:52 +0300

    Added delete product in product detail page

--- sts-frontend/src/pages/ProductDetail.jsx
+++ sts-frontend/src/pages/ProductDetail.jsx
@@ -3,11 +3,13 @@ import { useParams } from 'react-router-dom';
 import { authorizedRequest } from '../lib/api';
 import ProductCard from '../components/ProductsCard'
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function ProductDetail() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [errorMsg, setError] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -23,6 +25,17 @@ function ProductDetail() {
     fetchProduct();
   }, [id]);
 
+  const handleDelete = async () => {
+    try {
+      await authorizedRequest('delete', `/products/${id}/`);
+      navigate('/CustomerDashboard'); 
+    } catch (err) {
+      console.error(err);
+      setError('Failed to delete product.');
+    }
+  };
+  
+
   if (errorMsg) return <h2>{errorMsg}</h2>;
   if (!product) return <h2>Loading product...</h2>;
 
@@ -30,6 +43,7 @@ function ProductDetail() {
     <div>
     <ProductCard props={product} isDetail={true} />
     <Link to={`/products/${id}/edit`}> Edit Product </Link>
+    <button onClick={handleDelete}>Delete</button>
     </div>
   );
 }
